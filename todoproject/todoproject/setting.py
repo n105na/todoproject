5de95cd316556f92ff3df73571d2d21000c16f1f
n105na <ambenaini@gmail.com>
2024-09-01 19:25:32 +0100

    Initial commit of Django project and .gitignore file

---
+++ todoproject/todoproject/setting.py
@@ -0,0 +1,10 @@
+ DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tododb',
+        'USER': 'n105na',
+        'PASSWORD': '19051970',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
