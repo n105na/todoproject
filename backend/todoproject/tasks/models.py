9784ab30c61ba9b613e2f8caa1d1194f322bddad
n105na <ambenaini@gmail.com>
2024-09-04 16:14:38 +0100

    updating the tasks app name on the apps file

--- backend/todoproject/tasks/models.py
+++ backend/todoproject/tasks/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Task(models.Model):
+class Tasks(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
